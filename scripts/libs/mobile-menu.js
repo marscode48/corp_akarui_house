93d1198253ae7487656e334874695f8ae1b4eb23
Adachi Masakazu <marscode48@gmail.com>
2022-02-24 17:11:09 +0900

    JavaScriptでmobile-menuを作成
    修正

---
+++ scripts/libs/mobile-menu.js
@@ -0,0 +1,28 @@
+class MobileMenu {
+  constructor() {
+    this.DOM = {};
+    this.DOM.header = document.querySelector(".header");
+    this.DOM.hamburger = document.querySelector(".hamburger");
+    this.DOM.mask = document.querySelector("#mask");
+    this.DOM.navs = document.querySelectorAll("#navi a");
+    this.eventType = this._getEventType();
+    this._addEvent();
+  }
+
+  _getEventType() {
+    return window.ontouchstart ? "touchstart" : "click";
+  }
+
+  _toggle() {
+    this.DOM.header.classList.toggle("open");
+  }
+
+  _addEvent() {
+    this.DOM.hamburger.addEventListener(this.eventType, this._toggle.bind(this)
+    );
+    this.DOM.mask.addEventListener(this.eventType, this._toggle.bind(this));
+    this.DOM.navs.forEach((nav) => {
+      nav.addEventListener(this.eventType, this._toggle.bind(this));
+    });
+  }
+}
